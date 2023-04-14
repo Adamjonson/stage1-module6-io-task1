f75a308d0eb8a456618a1e612499195e009d58b7
Hp <adkhamjon.abd@gmail.com>
2023-04-15 01:29:19 +0200

    Completes the task

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,52 @@
 package com.epam.mjc.io;
 
-import java.io.File;
-
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
-    }
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] arrOfStr = null;
+
+        String line = null;
+        try(BufferedReader reader = new BufferedReader(new java.io.FileReader(file))){
+
+            String ls = System.getProperty("line.separator");
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append(ls);
+            }
+// delete the last new line separator
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            reader.close();
+
+            String str = stringBuilder.toString();
+            //System.out.println("Text" + str);
+            arrOfStr = str.split("\r");
+            //System.out.println("array");
+            //System.out.println(Arrays.toString(arrOfStr));
+            for (int i = 0; i< arrOfStr.length; i++){
+
+                String val = arrOfStr[i];
+                int spaceIndex = val.indexOf(" ");
+                String key = val.substring(0, spaceIndex);
+                String value = val.substring(spaceIndex+1, val.length());
+                //System.out.println("title" + key + " : " + value);
+                arrOfStr[i] = value;
+
+            }
+            //System.out.println(Arrays.toString(arrOfStr));
+        }catch (Exception e){
+            System.out.println("File not found");
+        }
+        Profile profile = new Profile(arrOfStr[0], Integer.parseInt(arrOfStr[1]), arrOfStr[2], Long.parseLong(arrOfStr[3]));
+
+// delete the last new line separator
+
+
+        return profile;
+}
+
 }
