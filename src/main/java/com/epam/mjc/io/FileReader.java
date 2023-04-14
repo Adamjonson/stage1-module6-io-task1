74dd28f678dd35d7ef65dadc0fe434a264badd50
Hp <adkhamjon.abd@gmail.com>
2023-04-15 01:34:14 +0200

    Changes the style

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -18,32 +18,26 @@ public class FileReader {
                 stringBuilder.append(line);
                 stringBuilder.append(ls);
             }
-// delete the last new line separator
             stringBuilder.deleteCharAt(stringBuilder.length() - 1);
             reader.close();
 
             String str = stringBuilder.toString();
-            //System.out.println("Text" + str);
             arrOfStr = str.split("\r");
-            //System.out.println("array");
-            //System.out.println(Arrays.toString(arrOfStr));
             for (int i = 0; i< arrOfStr.length; i++){
 
                 String val = arrOfStr[i];
                 int spaceIndex = val.indexOf(" ");
                 String key = val.substring(0, spaceIndex);
                 String value = val.substring(spaceIndex+1, val.length());
-                //System.out.println("title" + key + " : " + value);
                 arrOfStr[i] = value;
 
             }
-            //System.out.println(Arrays.toString(arrOfStr));
+
         }catch (Exception e){
             System.out.println("File not found");
         }
         Profile profile = new Profile(arrOfStr[0], Integer.parseInt(arrOfStr[1]), arrOfStr[2], Long.parseLong(arrOfStr[3]));
 
-// delete the last new line separator
 
 
         return profile;
